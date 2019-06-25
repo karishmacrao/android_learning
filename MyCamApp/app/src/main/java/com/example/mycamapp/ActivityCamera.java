1878d097f1a8a1624c4e252101d78c5739cee806
karishmacrao <karishmacrao@gmail.com>
2019-06-25 17:54:31 +0530

    Added camera app sample

---
+++ MyCamApp/app/src/main/java/com/example/mycamapp/ActivityCamera.java
@@ -0,0 +1,14 @@
+package com.example.mycamapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityCamera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
